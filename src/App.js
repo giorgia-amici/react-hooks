81e80628c4d8103e096422d96bdf8cba3d08df8f
Giorgia <giorgia1london@gmail.com>
2019-05-27 17:19:34 +0100

    React hooks playground

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-todo--item' style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}> 
+        { todo.text } 
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+        </div>
     </div>
-  );
+  )
+}
+
+function TodoForm({ addTodo }) {
+    //array destructuring syntax below 
+    //you have to pass an argument to useState which will be set as the original state
+    //for the first render. 
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //do not submit if empty string
+        if (!value) return;
+        addTodo(value);
+        setValue('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                className='input'
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
+    )
+}
+
+function App() {
+
+    //array destructuring syntax below <3
+    const [todos, setTodos] = useState([
+        {text: 'Book Gym', isCompleted: false},
+        {text: 'Update Calendar', isCompleted: false}
+    ]);
+
+    const addTodo = text => {
+        //use the spread operator to copy the content of the current todo list
+        const newTodos = [...todos, {text}];
+        setTodos(newTodos);    
+    }
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+
+        setTodos(newTodos);
+    } 
+
+    return (
+        <div className='app'>
+            <header className='app-header'></header>
+            <section className='app-todo'>
+                {todos.map((todo, index) => (
+                    <Todo 
+                        key={index} 
+                        todo={todo}
+                        index={index} 
+                        completeTodo={completeTodo}
+                    />
+                ))}
+            </section>
+            <TodoForm addTodo={addTodo} />
+        </div>
+    );
 }
 
 export default App;
+  
